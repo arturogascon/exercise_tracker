fdce13e111b67060fd3ce8112f4600288b2f1b3f
PapArturo <centinela90@gmail.com>
2020-09-25 17:14:08 -0500

    Preparing to deploy to Heroku

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -23,6 +28,11 @@ const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
+//Production Mode
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../build'));
+}
 
 app.listen(port, ()=> {
     console.log(`Server is running on port: ${port}`);
